VouDoo/MyRemoteManager

Use PowerShell automatic variable
Replace $env:USERPROFILE by $HOME PowerShell automatic variable.

Source/Classes/04_Inventory.ps1
@@ -24,7 +24,7 @@ class Inventory {
             )
             if ($Value) { return $Value }
         }
-        return Join-Path -Path $env:USERPROFILE -ChildPath "MyRemoteManager.json"
+        return Join-Path -Path $HOME -ChildPath "MyRemoteManager.json"
     }
 
     [void] ReadFile() {

VouDoo/Portal

Source/Classes/04_Inventory.ps1
@@ -24,7 +24,7 @@ class Inventory {
             )
             if ($Value) { return $Value }
         }
-        return Join-Path -Path $env:USERPROFILE -ChildPath "Portal.json"
+        return Join-Path -Path $HOME -ChildPath "Portal.json"
     }
 
     [void] ReadFile() {